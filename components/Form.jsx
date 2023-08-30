efac4e1322dba45aa9c3391d47aa4793836e8ca2
Rubén Bocanegra <rb@aconsoft.com>
2023-08-29 22:02:53 -0500

    posts listed

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import Link from 'next/link';
 
-const Form = () => {
+const Form = ({
+  type,
+  post,
+  setPost,
+  submitting,
+  handleSubmit
+}) => {
   return (
-    <div>Form</div>
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        <span className='blue_gradient'>{type} Post</span></h1>
+        <p className='desc text-left max-w-md'>
+        {type} and share amazing prompnts with the world,
+        and let your imagination run wild with any AI-powered platform
+        </p>
+        <form 
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2x1 flex flex-col gap-7 glassmorphism'
+        >
+            <label>
+              <span className='font-semibold text-base text-gray-700'>
+                Your AI Prompt
+              </span>
+            </label>
+            <textarea
+            value={post.prompt}
+            onChange={
+              (e)=>setPost({...post, prompt:e.target.value})
+            }
+            placeholder ="Write your prompt here"
+            className="form_textarea"
+            required
+            ></textarea>
+
+            <label>
+              <span className='font-semibold text-base text-gray-700'>
+                Tag
+                <span className='font-normal'>(#product, #webdevelopment)</span>
+              </span>
+            </label>
+            <input
+            value={post.tag}
+            onChange={
+              (e)=>setPost({...post, tag:e.target.value})
+            }
+            placeholder ="#tag"
+            className="form_input"
+            required
+            />
+
+            <div className='flex-end mx-3 mb-5 gap-4'>
+              <Link href="/" className='text-gray-500 text-sm'>
+              Cancel
+              </Link>
+              <button type="submit"
+              className='px-5 py-1.5 text-sm bg-orange-400	 rounded-full text-white'
+              disabled={submitting}>
+                {submitting ? `${type}...` : type}
+              </button>
+            </div>
+
+        </form>
+    </section>
+    
   )
 }
 
